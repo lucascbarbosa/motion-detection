9b7fa52ac072834f0c94b926b82a6e74c0c746e0
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2023-06-30 16:36:19 -0300

    updates

--- pre_process.m
+++ pre_process.m
@@ -5,6 +5,9 @@ clc;
 sinais_brutos_dir = './sinais_brutos/';
 sinais_filtrados_dir = './sinais_filtrados/';
 sinais_brutos_filenames = dir(sinais_brutos_dir);
+
+all_signals_emg = {};
+all_signals_acel = {};
 for i = 1:numel(sinais_brutos_filenames)
     if ~sinais_brutos_filenames(i).isdir
         file_path_bruto = fullfile(sinais_brutos_dir, sinais_brutos_filenames(i).name);
@@ -22,18 +25,22 @@ for i = 1:numel(sinais_brutos_filenames)
         bw = w0/35;
         [num,den] = iirnotch(w0,bw);
         signal_f = filtfilt(num,den, signal_emg_det);
-
+        
+        %% notch 60 Hz
+        w0 = 120/(fs/2);
+        bw = w0/35;
+        [num,den] = iirnotch(w0,bw);
+        signal_f = filtfilt(num,den, signal_f);
+        
         %% bandpass
         f_bp = [10, 400];
         order = 2;
         [b,a] = butter(order, f_bp/(fs/2), "bandpass");
 
         signal_f = filtfilt(b, a, signal_f);
-        [folder, name, ~] = fileparts(file_path_bruto);
-        file_path_filtrado = fullfile(sinais_filtrados_dir, [name '_emg.csv']);
-        csvwrite(file_path_filtrado, signal_f);
-           
-        %% acel
+        all_signals_emg{i} = signal_f;
+
+                %% acel
         signal_acel = all_data.ARQcanalesADC;   % acel
 
         %% filtro passa baixo
@@ -41,8 +48,8 @@ for i = 1:numel(sinais_brutos_filenames)
         order = 4;
         [b,a] = butter(order, f_lp/(fs/2), "low");  
         signal_f_acel = filtfilt(b, a, signal_acel');
-        file_path_filtrado = fullfile(sinais_filtrados_dir, [name '_acel.csv']);
-        csvwrite(file_path_filtrado, signal_f_acel);
+        all_signals_acel{i} = signal_f_acel;
+
 %         figure; % 7
 %         subplot(2,1,1);
 %         plot(t, signal_f);
@@ -52,3 +59,11 @@ for i = 1:numel(sinais_brutos_filenames)
 %         ma_fft_plot(signal_f, fs, 0);
     end 
 end
+all_signals_emg = cat(1, all_signals_emg{:});
+[folder, name, ~] = fileparts(file_path_bruto);
+file_path_filtrado = fullfile(sinais_filtrados_dir, 'clara_signals_emg.csv');
+csvwrite(file_path_filtrado, all_signals_emg);
+
+all_signals_acel = cat(1, all_signals_acel{:});
+file_path_filtrado = fullfile(sinais_filtrados_dir, 'clara_signals_acel.csv');
+csvwrite(file_path_filtrado, all_signals_acel);
