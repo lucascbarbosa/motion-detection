8a56efbbaac3e1aa4b9b7ab092cdffd3b195f2e5
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2023-07-02 20:20:23 -0300

    Update pre_process.m

--- pre_process.m
+++ pre_process.m
@@ -8,48 +8,46 @@ sinais_brutos_filenames = dir(sinais_brutos_dir);
 
 all_signals_emg = {};
 all_signals_acel = {};
-for i = 1:numel(sinais_brutos_filenames)
-    if ~sinais_brutos_filenames(i).isdir
-        file_path_bruto = fullfile(sinais_brutos_dir, sinais_brutos_filenames(i).name);
-        all_data = Open_File_MAdq(file_path_bruto);
-        %% read signals
-        fs = all_data.Fs; % Hz
-        signal_emg = all_data.ARQdigCal(1:3,:); % emg
-        
-        n_amostras = length(signal_emg);
-        t = [0: n_amostras - 1]/fs;
-        %% detrend
-        signal_emg_det = detrend(signal_emg');
-        %% notch 60 Hz
-        w0 = 60/(fs/2);
-        bw = w0/35;
-        [num,den] = iirnotch(w0,bw);
-        signal_f = filtfilt(num,den, signal_emg_det);
-        
-        %% notch 60 Hz
-        w0 = 120/(fs/2);
-        bw = w0/35;
-        [num,den] = iirnotch(w0,bw);
-        signal_f = filtfilt(num,den, signal_f);
-        
-        %% bandpass
-        f_bp = [10, 400];
-        order = 2;
-        [b,a] = butter(order, f_bp/(fs/2), "bandpass");
+for i = 3:numel(sinais_brutos_filenames)
+    file_path_bruto = fullfile(sinais_brutos_dir, sinais_brutos_filenames(i).name);
+    all_data = Open_File_MAdq(file_path_bruto);
+    %% read signals
+    fs = all_data.Fs; % Hz
+    signal_emg = all_data.ARQdigCal(1:3,:); % emg 
 
-        signal_f = filtfilt(b, a, signal_f);
-        all_signals_emg{i} = signal_f;
+    n_amostras = length(signal_emg);
+    t = [0: n_amostras - 1]/fs;
+    %% detrend
+    signal_emg_det = detrend(signal_emg');
+    %% notch 60 Hz
+    w0 = 60/(fs/2);
+    bw = w0/35;
+    [num,den] = iirnotch(w0,bw);
+    signal_f = filtfilt(num,den, signal_emg_det);
 
-                %% acel
-        signal_acel = all_data.ARQcanalesADC;   % acel
+    %% notch 60 Hz
+    w0 = 120/(fs/2);
+    bw = w0/35;
+    [num,den] = iirnotch(w0,bw);
+    signal_f = filtfilt(num,den, signal_f);
 
-        %% filtro passa baixo
-        f_lp = 20;  % Hz
-        order = 4;
-        [b,a] = butter(order, f_lp/(fs/2), "low");  
-        signal_f_acel = filtfilt(b, a, signal_acel');
-        all_signals_acel{i} = signal_f_acel;
+    %% bandpass
+    f_bp = [10, 400];
+    order = 2;
+    [b,a] = butter(order, f_bp/(fs/2), "bandpass");
 
+    signal_f = filtfilt(b, a, signal_f);
+    all_signals_emg{i-2} = signal_f;
+
+            %% acel
+    signal_acel = all_data.ARQcanalesADC;   % acel
+
+    %% filtro passa baixo
+    f_lp = 20;  % Hz
+    order = 4;
+    [b,a] = butter(order, f_lp/(fs/2), "low");  
+    signal_f_acel = filtfilt(b, a, signal_acel');
+    all_signals_acel{i-2} = signal_f_acel;
 %         figure; % 7
 %         subplot(2,1,1);
 %         plot(t, signal_f);
@@ -57,7 +55,6 @@ for i = 1:numel(sinais_brutos_filenames)
 %         legend('ch1', 'ch2', 'ch3');
 %         subplot(2,1,2);
 %         ma_fft_plot(signal_f, fs, 0);
-    end 
 end
 all_signals_emg = cat(1, all_signals_emg{:});
 [folder, name, ~] = fileparts(file_path_bruto);
